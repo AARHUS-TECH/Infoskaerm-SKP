782e2012c903c84c247dca71e08759f0bef55790
Eray Tecer <60657949+Tekop1223@users.noreply.github.com>
2022-03-30 12:39:57 +0200

    fade fix

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -35,52 +35,22 @@ document.addEventListener("DOMContentLoaded", function(event) {
 			 const opslagdata = JSON.parse(svar);
 			 const opslag = opslagdata.news
 				
-			 	for(var i = 0; i <= opslag.length; i++) {
-					const titleText = opslag[i].header
-					const bodyText = opslag[i].body
-					const startText = opslag[i].startdate
-					const endText = opslag[i].enddate
-    				
-					function rotateTerm() { // using jquerry to make it fade make it change to the next set of data
+					function fadingData() { // using jquerry to make it fade and make it change to the next set of data
 						var headernumber = $(".info_titeltext").data("header") || 0;
 						var bodynumber = $(".info_text").data("body") || 0;
 						var startnumber = $(".startdateholder").data("startdate") || 0;
 						var endnumber = $(".enddateholder").data("enddate") || 0;
-    					$(".info_titeltext")
-      						.data("header", headernumber == opslag.length - 1 ? 0 : headernumber + 1)
-							.html(opslag[headernumber].header)
-      						.fadeIn(500)	
-      						.delay(20000) 
-      						.fadeOut(500);
 
-						$(".info_text")
-      						.data("body", bodynumber == opslag.length - 1 ? 0 : bodynumber + 1)
-							.html(opslag[bodynumber].body)
-      						.fadeIn(500)	
-      						.delay(20000) 
-      						.fadeOut(500);
-
-						$(".startdateholder")
-      						.data("startdate", startnumber == opslag.length - 1 ? 0 : startnumber + 1)
-							.html(opslag[startnumber].startdate)
-      						.fadeIn(500)	
-      						.delay(20000) 
-      						.fadeOut(500);
-
-						$(".enddateholder")
-      						.data("enddate", endnumber == opslag.length - 1 ? 0 : endnumber + 1)
-							.html(opslag[endnumber].enddate)
-      						.fadeIn(500)	
-      						.delay(20000) 
-      						.fadeOut(500, rotateTerm);						
+						
+    					$(".info_titeltext").data("header", headernumber == opslag.length - 1 ? 0 : headernumber + 1).html(opslag[headernumber].header)
+						$(".info_text").data("body", bodynumber == opslag.length - 1 ? 0 : bodynumber + 1).html(opslag[bodynumber].body);
+						$(".startdateholder").data("startdate", startnumber == opslag.length - 1 ? 0 : startnumber + 1).html(opslag[startnumber].startdate);
+						$(".enddateholder").data("enddate", endnumber == opslag.length - 1 ? 0 : endnumber + 1).html(opslag[endnumber].enddate);
+						$(".info_titeltext,.info_text").fadeIn(500).delay(2000).fadeOut(500, fadingData);
   					}
-  					rotateTerm();
-			
-					document.querySelector(".info_titeltext").innerHTML = titleText
-					document.querySelector(".info_text").innerHTML = bodyText
-					document.querySelector(".startdateholder").innerHTML = startText
-					document.querySelector(".enddateholder").innerHTML = endText
-				}
+  					fadingData();
+		
+				
 				
 
   		};
