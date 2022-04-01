c0e34a0ee8e6c4ad33f78e48101eecbe3a13d1c0
Eray Tecer <60657949+Tekop1223@users.noreply.github.com>
2022-04-01 10:18:13 +0200

    01-04-2022
    opslag fade fix

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -34,13 +34,12 @@ document.addEventListener("DOMContentLoaded", function(event) {
 		 	 const svar = xhttp.responseText;
 			 const opslagdata = JSON.parse(svar);
 			 const opslag = opslagdata.news
-				
+
 					function fadingData() { // using jquerry to make it fade and make it change to the next set of data
 						var headernumber = $(".info_titeltext").data("header") || 0;
 						var bodynumber = $(".info_text").data("body") || 0;
 						var startnumber = $(".startdateholder").data("startdate") || 0;
-						var endnumber = $(".enddateholder").data("enddate") || 0;
-
+						var endnumber = $(".enddateholder".substring(4,15)).data("enddate") || 0;
 						
     					$(".info_titeltext").data("header", headernumber == opslag.length - 1 ? 0 : headernumber + 1).html(opslag[headernumber].header)
 						$(".info_text").data("body", bodynumber == opslag.length - 1 ? 0 : bodynumber + 1).html(opslag[bodynumber].body);
