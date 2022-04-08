05204c06eca387019458570b56020fc2c32148c5
Eray Tecer <60657949+Tekop1223@users.noreply.github.com>
2022-04-08 14:13:48 +0200

    rettet manglerne tegn

--- frontend/js/fadeinfadeout.js
+++ frontend/js/fadeinfadeout.js
@@ -2,13 +2,13 @@ var terms = [
   `<img src="images/Ulrich.png" class="billeder">
     <p>
     <p class="instruktoer_title"> Ulrich kan kontaktes p&aring;:</p>
-    mail: umfaarhustech.dk<br> Telefon: 61987304
+    mail: umf@aarhustech.dk<br> Telefon: 61987304
     </p><br>
 
     <img src="images/Karsten.png" class="billeder">
     <p>
     <p class="instruktoer_title">Karsten kan kontaktes p&aring;:</p>
-    mail: krsaarhustech.dk<br> Telefon: 22234565
+    mail: krs@aarhustech.dk<br> Telefon: 22234565
     </p>
     <br />
     <img src="images/Simon.png" class="billeder" />
@@ -18,7 +18,7 @@ var terms = [
     </p>`,
   `Alt hvad du skal vide om skp finder du her: <img src="images/skpdatait.png" class="QR_kode"> <br><br> www.ats-skpdatait.dk 
    <br> <br> <br> Hvis du mangler noget bestiller du det her: <img src="images/skpinventar.png" class="QR_kode"> <br> <br> www.inventar.ats-skpdatait.dk
-   <br> <br> <br> du kan se en plan over skolen her: <img src="images/skpfindvej.png" class="QR_kode"><br><br> www.findvej.ats-skpdatait.dk`,
+   <br> <br> <br> Du kan se en plan over skolen her: <img src="images/skpfindvej.png" class="QR_kode"><br><br> www.findvej.ats-skpdatait.dk`,
   `<p class="instruktoer_title">Servicedesk</p><br>Du kan henvende dig i Servicedesk for at få hjælp de mest almindelige problemer. Også hvis du har problemer med at logge. Servicedesk ligger på 3. sal i lokale AS318<br> <br> <br>  <br>
    <p class="instruktoer_title">Techdesk</p><br>Når du går på hovedforløb kan du bestille udstyr gennem inventar.ats-skpdatait.dk. De vil så finde dit udstyr frem og udlevere det. Techdesk er placeret på 2. sal på gangen med hovedforløbet i lokale AC206`,
 ];
