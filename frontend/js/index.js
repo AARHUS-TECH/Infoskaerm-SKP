3acc69df15ab15b34012ca3d426a0d92e943b43a
ATS-SKP-DataIT <krs@aarhustech.dk>
2022-04-22 09:17:25 +0200

    Better slide timing

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -36,16 +36,20 @@ document.addEventListener("DOMContentLoaded", function(event) {
 			 const opslag = opslagdata.news
 
 					function fadingData() { // using jquerry to make it fade and make it change to the next set of data
+						setInterval(function () {
 						var headernumber = $(".info_titeltext").data("header") || 0;
 						var bodynumber = $(".info_text").data("body") || 0;
-						var startnumber = $(".startdateholder").data("startdate") || 0;
-						var endnumber = $(".enddateholder".substring(4,15)).data("enddate") || 0;
+					 	var startnumber = $(".startdateholder").data("startdate") || 0;
+					 	var endnumber = $(".enddateholder").data("enddate") || 0;
 						
-    					$(".info_titeltext").data("header", headernumber == opslag.length - 1 ? 0 : headernumber + 1).html(opslag[headernumber].header)
-						$(".info_text").data("body", bodynumber == opslag.length - 1 ? 0 : bodynumber + 1).html(opslag[bodynumber].body);
-						$(".startdateholder").data("startdate", startnumber == opslag.length - 1 ? 0 : startnumber + 1).html(opslag[startnumber].startdate);
-						$(".enddateholder").data("enddate", endnumber == opslag.length - 1 ? 0 : endnumber + 1).html(opslag[endnumber].enddate);
-						$(".info_titeltext,.info_text").fadeIn(500).delay(2000).fadeOut(500, fadingData);
+    				 	$(".info_titeltext").data("header", headernumber == opslag.length - 1 ? 0 : headernumber + 1).html(opslag[headernumber].header)
+					 	$(".info_text").data("body", bodynumber == opslag.length - 1 ? 0 : bodynumber + 1).html(opslag[bodynumber].body);
+					 	$(".startdateholder").data("startdate", startnumber == opslag.length - 1 ? 0 : startnumber + 1).html(opslag[startnumber].startdate);
+					 	$(".enddateholder").data("enddate", endnumber == opslag.length - 1 ? 0 : endnumber + 1).html(opslag[endnumber].enddate);
+					 	 
+        				$(".info_titeltext,.info_text").fadeIn(500).delay(2000).fadeOut(500);
+    					}, 3100);
+
   					}
   					fadingData();
 		
