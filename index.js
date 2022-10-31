8a62018e0800ad921454d01c4ce74773374e98d6
Schou <64755996+Sch0u@users.noreply.github.com>
2022-10-31 10:39:23 +0100

    test

--- index.js
+++ index.js
@@ -159,7 +159,7 @@ async function loadSite() {
         let session = req.session
         var personList = con.getAllUsers()
         if (session.key) {
-            res.render('admin/database.pug', { "personList": personList }, {"Test": "test"})
+            res.render('admin/database', { personList: personList, message: 'Hello there!', userID: session.key })
         } else {
             req.session.verificationFailed = true // Check to make sure fail message is shown
             res.redirect("/login")
